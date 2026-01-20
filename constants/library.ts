ef316b3fedbdb0e5cd8a62fd0eed67044fbc3a12
WakamoLover <kirarachanii77@gmail.com>
2026-01-20 21:45:02 +0900

    Update library.ts

--- constants/library.ts
+++ constants/library.ts
@@ -6,9 +6,165 @@ export const LIBRARY_ITEMS: Omit<Post, 'id'>[] = [
   {
     title: '',
     description: '',
-    coverImage: 'https://pbs.twimg.com/media/G9dqE7Aa0AAnDmW?format=jpg&name=4096x4096',
+    coverImage: '',
     type: ContentType.IMAGE,
     category: 'Illustrator',
-    externalLink: 'https://x.com/SilverPalace_EN/status/2006561152202060147?s=20'
+    externalLink: ''
+  },
+  
+// Cosplpayer
+  {
+    title: '封疆疆v',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/GDlZN35W4AAevPn?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/Dolly48226855/media'
+  },
+  {
+    title: 'Josette',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/GL6LdoZaQAAueeC?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/josetteswan/media'
+  },
+  {
+    title: 'taru',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/GS2HinMbYAAPrf4?format=jpg&name=4096x4096',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/taru153/media'
+  },
+  {
+    title: 'reng',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/Ggr4iC8bgAAs1oP?format=jpg&name=4096x4096',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/idtNothinglose/media'
+  },
+  {
+    title: '兎団子',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/G8NDwykaYAAKoCE?format=jpg&name=4096x4096',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/usadango27/media'
+  },
+  {
+    title: 'Ely',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/G75hbjlaYAAkQIA?format=jpg&name=4096x4096',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/Ely_eee/media'
+  },
+  {
+    title: '贤儿sherry',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/GMa8grEa8AAeeO-?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/sherryken777/media'
+  },
+  {
+    title: '铁板欧尼酱',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/FxRPyU5aMAAoQBO?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/tiebanonini/media'
+  },
+  {
+    title: '大薯条',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/G9ybzfHbgAAGQle?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/kanek1ww/media'
+  },
+  {
+    title: 'ふちどり',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/Gv-R0KdXgAAK967?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/Futidori_came/media'
+  },
+  {
+    title: '九柒喵aaa',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/G7UZt-PaAAAjKaV?format=jpg&name=medium',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/jiuqiqiqiqim/media'
+  },
+  {
+    title: '纸悦',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/G8LTa4fbYAAnP-q?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/Etsuko827/media'
+  },
+  {
+    title: 'xier',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/G5H8oicX0AAjKNC?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/xier33/media'
+  },
+  {
+    title: '月海つくね',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/G4gEexPaMAAgYmi?format=jpg&name=4096x4096',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/XaiabP/media'
+  },
+  },
+  {
+    title: '憐',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/G7a8b8Wb0AIBDMv?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/ren_neco222/media'
+  },
+  },
+  {
+    title: 'スッキ',
+    description: '',
+    coverImage: 'https://pbs.twimg.com/media/G6HP-i0agAAbQjZ?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://pbs.twimg.com/media/GfYIZ2hbMAAJiI0?format=jpg&name=large'
+  },
+  {
+    title: 'かしゅーわ',
+    description: 'Photographer',
+    coverImage: 'https://pbs.twimg.com/media/G753UN4aUAENjRN?format=jpg&name=4096x4096',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/kasyuuwa/media'
+  },
+  {
+    title: 'みやび',
+    description: 'Photographer',
+    coverImage: 'https://pbs.twimg.com/media/G8H4zNAaMAEvU82?format=jpg&name=4096x4096',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/tnsk6400/media'
+  },
+  {
+    title: '飞鱼Fiyu',
+    description: 'Photographer',
+    coverImage: 'https://pbs.twimg.com/media/G8iUzlOaMAEBqEH?format=jpg&name=large',
+    type: ContentType.IMAGE,
+    category: 'Cosplayer',
+    externalLink: 'https://x.com/UKYEJFT/media'
   },
 ];
