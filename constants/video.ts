a787eb61d8773a3b6b1473de032d89d0eb9ee7c6
WakamoLover <kirarachanii77@gmail.com>
2026-08-10 21:10:44 +0900

    Your message

--- constants/video.ts
+++ constants/video.ts
@@ -7,7 +7,7 @@ export const VIDEO_ITEMS: Omit<Post, 'id'>[] = [
   {
     title: 'Unknown',
     description: '',
-    coverImage: 'https://i.ytimg.com/vi/QwExFKxU5ok/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBhNm2QTFNgyb-sXLQZ8ujWu9U84Ahttps://i.ytimg.com/an_webp/QwExFKxU5ok/mqdefault_6s.webp?du=3000&sqp=CIPv5tMG&rs=AOn4CLB2cc6-loEMi2APSWe5wLR0gjzmMghttps://i.ytimg.com/vi/QwExFKxU5ok/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBhNm2QTFNgyb-sXLQZ8ujWu9U84A',
+    coverImage: 'https://i.ytimg.com/vi/QwExFKxU5ok/hqdefault.jpg',
     type: ContentType.VIDEO,
     category: 'Youtube',
     channelUrl: 'https://youtu.be/QwExFKxU5ok'
